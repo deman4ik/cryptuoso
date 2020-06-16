3f0256cf74e9fd40e82cbbd3b821b26d747aa305
Dmitriy Shatalov <deman4ik@gmail.com>
2020-06-16 12:15:22 +0300

    feat: Add service lib

---
+++ libs/service/src/lib/BaseService.ts
@@ -0,0 +1,114 @@
+import "dotenv-safe/config";
+import { createLightship, LightshipType } from "lightship";
+import Redis from "ioredis";
+import logger, { Logger } from "@cryptuoso/logger";
+
+export interface BaseServiceConfig {
+    name: string;
+}
+
+export class BaseService {
+    #log: Logger;
+    #lightship: LightshipType;
+    #name: string;
+    #onServiceStart: { (): Promise<void> }[] = [];
+    #onServiceStop: { (): Promise<void> }[] = [];
+    #redisConnection: Redis.Redis;
+
+    constructor(config: BaseServiceConfig) {
+        try {
+            this.#log = logger;
+            this.#lightship = createLightship({
+                port: +process.env.LS_PORT || 9000,
+                detectKubernetes: process.env.NODE_ENV === "production"
+            });
+            this.#lightship.registerShutdownHandler(this.#stopService.bind(this));
+
+            this.#name = config.name || process.env.SERVICE;
+            process.on("uncaughtException", this.#handleUncaughtException.bind(this));
+            process.on("unhandledRejection", this.#handleUnhandledRejection.bind(this));
+        } catch (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    }
+
+    get log() {
+        return this.#log;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    #handleUncaughtException = (err: Error) => {
+        this.#log.error(err, "uncaughtException");
+    };
+    #handleUnhandledRejection = (err: Error) => {
+        this.#log.error(err, "unhandledRejection");
+    };
+
+    #addOnStartHandler = async (func: () => Promise<void>) => {
+        if (func && typeof func === "function") this.#onServiceStart.push(func.bind(this));
+    };
+
+    get addOnStartHandler() {
+        return this.#addOnStartHandler;
+    }
+
+    #addOnStopHandler = async (func: () => Promise<void>) => {
+        if (func && typeof func === "function") this.#onServiceStop.push(func.bind(this));
+    };
+
+    get addOnStopHandler() {
+        return this.#addOnStopHandler;
+    }
+
+    #startService = async () => {
+        this.#log.info(`Starting ${this.#name} service...`);
+        try {
+            if (this.#onServiceStart.length > 0) {
+                for (const onStartFunc of this.#onServiceStart) {
+                    await onStartFunc();
+                }
+            } else {
+                await Promise.resolve();
+            }
+            this.#redisConnection = new Redis(
+                process.env.REDIS_CS //,{enableReadyCheck: false}
+            );
+            this.#lightship.signalReady();
+            this.#log.info(`Started ${this.#name} service`);
+        } catch (err) {
+            console.error(err);
+            this.#log.error(err, `Failed to start ${this.#name} service`);
+            process.exit(1);
+        }
+    };
+
+    get startService() {
+        return this.#startService;
+    }
+
+    #stopService = async () => {
+        this.#log.info(`Stopping ${this.#name} service...`);
+        try {
+            if (this.#onServiceStop.length > 0) {
+                for (const onStopFunc of this.#onServiceStop) {
+                    await onStopFunc();
+                }
+            }
+        } catch (err) {
+            this.#log.error(err, `Failed to correctly stop ${this.#name} service`);
+            process.exit(1);
+        }
+    };
+
+    get stopService() {
+        return this.#stopService;
+    }
+
+    get redis() {
+        return this.#redisConnection;
+    }
+}
---
+++ libs/service/src/lib/HTTPService.ts
@@ -0,0 +1,174 @@
+import restana, { Service, Protocol, Request, Response, RequestHandler } from "restana";
+import ifunless from "middleware-if-unless";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import Validator, { ValidationSchema, ValidationError } from "fastest-validator";
+import { BaseService, BaseServiceConfig } from "./BaseService";
+import { ActionsHandlerError } from "@cryptuoso/errors";
+
+//TODO: req/res typings
+export interface ActionPayload {
+    action: {
+        name: string;
+    };
+    input: { [key: string]: any };
+    session_variables: { [key: string]: string };
+}
+
+export interface HTTPServiceConfig extends BaseServiceConfig {
+    port?: number;
+}
+
+export class HTTPService extends BaseService {
+    private _iu: any;
+    private _v: Validator;
+    private _port: number;
+    private _server: Service<Protocol.HTTP>;
+    private _routes: {
+        [key: string]: {
+            validate: (value: any) => true | ValidationError[];
+            auth: boolean;
+            roles: string[];
+        };
+    } = {};
+    constructor(config: HTTPServiceConfig) {
+        super(config);
+        try {
+            this._iu = ifunless();
+            this._v = new Validator();
+            this._port = config.port || +process.env.PORT || +process.env.NODE_PORT || 3000;
+            this._server = restana({
+                errorHandler: this._errorHandler
+            });
+            this._server.use(<RequestHandler<Protocol.HTTP>>helmet());
+            this._server.use(bodyParser.json());
+            this._server.use(this._checkApiKey.bind(this));
+            this._server.use(
+                "/actions",
+                this._iu(this._checkValidation.bind(this)).iff(
+                    (req: any) => this._routes[req.url] && this._routes[req.url].validate
+                )
+            );
+            this._server.use(
+                "/actions",
+                this._iu(this._checkAuth.bind(this)).iff(
+                    (req: any) => this._routes[req.url] && this._routes[req.url].auth
+                )
+            );
+
+            this.addOnStartHandler(this._startServer);
+            this.addOnStopHandler(this._stopServer);
+        } catch (err) {
+            this.log.error(err, "While consctructing HTTPService");
+            process.exit(1);
+        }
+    }
+
+    private async _startServer() {
+        this._server.start(this._port, "0.0.0.0");
+        this.log.info(`HTTP listening on ${this._port}`);
+        if (this._server.routes().length > 0) {
+            this.log.info(`with routes \n${this._server.routes().join("\n")}`);
+        }
+    }
+
+    private async _stopServer() {
+        this._server.close();
+    }
+
+    private _checkApiKey(req: Request<Protocol>, res: Response<Protocol>, next: (err?: Error) => void) {
+        if (!req.headers["x-api-key"] || req.headers["x-api-key"] !== process.env.API_KEY) {
+            throw new ActionsHandlerError("Forbidden: Invalid API Key", null, "FORBIDDEN", 403);
+        }
+        next();
+    }
+
+    private _checkValidation(req: Request<Protocol>, res: Response<Protocol>, next: (err?: Error) => void) {
+        const validationErrors = this._routes[req.url].validate(req.body);
+        if (validationErrors === true) next();
+        else
+            throw new ActionsHandlerError(
+                validationErrors.map((e) => e.message).join(" "),
+                { validationErrors },
+                "VALIDATION",
+                400
+            );
+    }
+
+    private _checkAuth(req: any, res: Response<Protocol>, next: (err?: Error) => void) {
+        try {
+            const userId = req.body.session_variables["x-hasura-user-id"];
+            if (!userId)
+                throw new ActionsHandlerError("Unauthorized: Invalid session variables", null, "UNAUTHORIZED", 401);
+
+            const role = req.body.session_variables["x-hasura-role"];
+
+            if (!this._routes[req.url].roles.includes(role))
+                throw new ActionsHandlerError("Forbidden: Invalid role", null, "FORBIDDEN", 403);
+
+            next();
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    private _errorHandler(err: Error, req: any, res: Response<Protocol>) {
+        req.log.warn(err);
+        if (err instanceof ActionsHandlerError) {
+            res.send(err.response, err.statusCode);
+        } else {
+            res.send(
+                {
+                    message: err.message,
+                    code: err.name
+                },
+                400
+            );
+        }
+    }
+
+    private _createRoutes(
+        routes: {
+            name: string;
+            handler: (req: any, res: any) => Promise<any>;
+            auth?: boolean;
+            roles?: string[];
+            inputSchema?: ValidationSchema;
+        }[]
+    ) {
+        routes.forEach((route) => {
+            let { name, handler, auth, roles, inputSchema } = route;
+            if (!name) throw new Error("Route name is required");
+            if (!handler && typeof handler !== "function") throw new Error("Route handler must be a function");
+            auth = auth || false;
+            roles = roles || [];
+            inputSchema = inputSchema || undefined;
+            const schema: ValidationSchema = {
+                action: {
+                    type: "object",
+                    props: {
+                        name: { type: "equal", value: name }
+                    }
+                },
+                input: { type: "object", props: inputSchema },
+                session_variables: {
+                    type: "object",
+                    props: {
+                        "x-hasura-user-id": { type: "string", optional: !auth },
+                        "x-hasura-role": { type: "string", optional: roles.length === 0 }
+                    }
+                }
+            };
+            this._routes[`/actions/${name}`] = {
+                validate: this._v.compile(schema),
+                auth,
+                roles
+            };
+            this._server.post(`/actions/${name}`, handler.bind(this));
+        });
+    }
+
+    get createRoutes() {
+        return this._createRoutes;
+    }
+}
