45c7efae1ec81bb0c3e20e2f54fb17b6f7f01725
Dmitriy Shatalov <deman4ik@gmail.com>
2020-06-16 04:31:37 +0300

    feat: Add custom errors lib

---
+++ libs/errors/src/lib/errors.spec.ts
@@ -0,0 +1,23 @@
+import { BaseError, ActionsHandlerError } from "./errors";
+
+describe("errors", () => {
+    it("should generate BaseError", () => {
+        const error = new BaseError("error message", { foo: "bar" }, "MY_ERROR");
+        expect(error instanceof Error).toBeTruthy();
+        expect(error.message).toBe("error message");
+        expect(error.meta).toStrictEqual({ foo: "bar" });
+        expect(error.type).toBe("MY_ERROR");
+    });
+    it("should generate ActionsHandlerError", () => {
+        const error = new ActionsHandlerError("error message", { foo: "bar" }, "MY_ERROR", 400);
+        expect(error instanceof Error).toBeTruthy();
+        expect(error.message).toBe("error message");
+        expect(error.meta).toStrictEqual({ foo: "bar" });
+        expect(error.type).toBe("MY_ERROR");
+        expect(error.statusCode).toBe(400);
+        expect(error.response).toStrictEqual({
+            message: "error message",
+            code: "MY_ERROR"
+        });
+    });
+});
