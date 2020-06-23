3fc53f432799477b7fc94068e71e6cbe8e9dbae2
Dmitriy Shatalov <deman4ik@gmail.com>
2020-06-24 02:48:35 +0300

    try

--- deployments/script.sh
+++ deployments/script.sh
@@ -1,5 +1,5 @@
 #!bash
-export SERVICES_LIST="$(nx affected:apps --plain --base=$(git rev-parse HEAD~1))"
+export SERVICES_LIST="$(nx affected:apps --plain --base=master~1 --head=origin/master)"
 services=($SERVICES_LIST) 
 for i in "${services[@]}"; do echo "$i"; done
 echo "$SERVICES_LIST"
