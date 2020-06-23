2989c854e5965e8032c34cc070716f5cb8c363f9
Dmitriy Shatalov <deman4ik@gmail.com>
2020-06-24 02:27:42 +0300

    nx base

--- deployments/script.sh
+++ deployments/script.sh
@@ -1,5 +1,5 @@
 #!bash
-export SERVICES_LIST="$(nx affected:apps --plain)"
+export SERVICES_LIST="$(nx affected:apps --plain --base=$NX_BASE)"
 services=($SERVICES_LIST) 
 for i in "${services[@]}"; do echo "$i"; done
 echo "$SERVICES_LIST"
