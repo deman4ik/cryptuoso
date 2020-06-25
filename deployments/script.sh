f6aaafb80296bf57418c544b2e5e3a4b2a982c47
Dmitriy Shatalov <deman4ik@gmail.com>
2020-06-25 17:28:52 +0300

    new script

--- deployments/script.sh
+++ deployments/script.sh
@@ -1,7 +1,5 @@
 #!bash
-base=$(git rev-list -n 1 $(git tag | tail -1)) 
-echo "nx affected:apps --plain --base=$base"
-SERVICES_LIST=$(nx affected:apps --plain --base=$base)
+SERVICES_LIST=$(node ./node_modules/@nrwl/cli/bin/nx.js affected:apps --plain --all  )
 echo "$SERVICES_LIST"
 services=($SERVICES_LIST) 
 for i in "${services[@]}"; do echo "$i"; done
