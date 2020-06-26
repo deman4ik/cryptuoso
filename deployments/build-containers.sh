960ee11dc08d891fe8b5f2a4e36093d5422fe955
Dmitriy Shatalov <deman4ik@gmail.com>
2020-06-26 03:40:09 +0300

    build

--- deployments/build-containers.sh
+++
@@ -1,3 +0,0 @@
-#!bash
-services=(${{ steps.services.outputs.list }})
-for i in "${services[@]}"; do docker build --build-arg GITHUB_SSH_KEY="${{ secrets.GITHUB_SSH_KEY }}" -t registry.digitalocean.com/cpz/$i:$(echo $GITHUB_SHA | head -c7) .; done 
--- deployments/script.sh
+++
@@ -1,5 +0,0 @@
-#!bash
-SERVICES_LIST=$(node ./node_modules/@nrwl/cli/bin/nx.js affected:apps --plain --all  )
-echo "$SERVICES_LIST"
-services=($SERVICES_LIST) 
-for i in "${services[@]}"; do echo "$i"; done
