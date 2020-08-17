4f2735b3a5c9f1f2f70f01f5e3223c04ef76cd14
Dmitriy Shatalov <deman4ik@gmail.com>
2020-08-17 15:16:54 +0300

    feat(postgres): date parsers

--- libs/postgres/src/lib/postgres.ts
+++ libs/postgres/src/lib/postgres.ts
@@ -1,5 +1,6 @@
-import { createPool, sql, ClientConfigurationInputType } from "slonik";
+import { createPool, sql, ClientConfigurationInputType, createTypeParserPreset } from "slonik";
 import { createFieldNameTransformationInterceptor } from "slonik-interceptor-field-name-transformation";
+import dayjs from "@cryptuoso/dayjs";
 import { prepareUnnest } from "./helpers";
 
 const interceptors = [
@@ -7,13 +8,30 @@ const interceptors = [
         format: "CAMEL_CASE"
     })
 ];
+const parseDate = (value: string) => (!value ? value : dayjs.utc(value).toISOString());
 
+const typeParsers = [
+    ...createTypeParserPreset(),
+    {
+        name: "date",
+        parse: parseDate
+    },
+    {
+        name: "timestamp",
+        parse: parseDate
+    },
+    {
+        name: "timestamptz",
+        parse: parseDate
+    }
+];
 const config: ClientConfigurationInputType = {
     connectionRetryLimit: 5,
     connectionTimeout: 10000,
     idleTimeout: 3000,
     maximumPoolSize: 16,
-    interceptors
+    interceptors,
+    typeParsers
 };
 
 const pg = createPool(process.env.PGCS, config);
