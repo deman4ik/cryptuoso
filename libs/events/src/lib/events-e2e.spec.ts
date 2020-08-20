58482dbce0650a6a74a1de9f1f44b4cc36ac153d
VanillinT <tominec98@gmail.com>
2020-08-20 18:41:15 +0300

    e2e refactor

--- libs/events/src/lib/events-e2e.spec.ts
+++ libs/events/src/lib/events-e2e.spec.ts
@@ -4,7 +4,17 @@ import { Events } from "./events";
 import { sleep } from "@cryptuoso/helpers";
 import { ValidationSchema } from "fastest-validator";
 
-jest.setTimeout(15000);
+jest.setTimeout(20000);
+const itif = (name: string, condition: () => boolean, cb: Function) => {
+    it(name, (done) => {
+        if (condition()) {
+            cb(done);
+        } else {
+            console.warn(`[skipped]: ${name}`);
+            done();
+        }
+    });
+};
 const calcSchema: ValidationSchema = {
     info: "string",
     numbers: { type: "array", items: "number" }
@@ -13,163 +23,142 @@ const calcSchema: ValidationSchema = {
 describe("E2E test", () => {
     const lightship = createLightship();
     const eventConfig = {
-        blockTimeout: 20,
-        pendingRetryRate: -1,
-        pendingMinIdleTime: 20
+        blockTimeout: 50,
+        pendingMinIdleTime: 50,
+        pendingRetryRate: 5,
+        pendingMaxRetries: 1
     };
     let redis: Redis.Redis;
     describe("Test connection to redis instance", () => {
         it("Should connect and assign a value to redis variable", (done) => {
             redis = new Redis({ port: 6379, host: "127.0.0.1" })
                 .on("error", (err) => {
-                    console.log("Connection to redis could not be established.\n" + err);
+                    console.warn("Connection to redis could not be established.\n" + err);
+                    redis.quit();
+                    redis = null;
                     done();
                 })
                 .on("end", () => {
-                    console.log("Error connecting to redis instance.");
+                    console.warn("Error connecting to redis instance.");
+                    redis.quit();
+                    redis = null;
                     done();
                 })
                 .on("ready", async () => {
-                    await redis.flushall();
                     done();
                 });
         });
     });
     describe("Test Events workflow", () => {
-        it("Should have redis initialized", () => {
-            expect(redis).not.toBe(null);
-        });
+        itif(
+            "Should have redis initialized",
+            () => redis != null,
+            (done: Function) => {
+                console.log("Redis is online");
+                done();
+            }
+        );
 
-        const dataSupplierHandler = jest.fn(async ({ foo, bar }) => {
+        const deliverHandler = jest.fn(async ({ foo, bar }) => {
                 console.log("foo is " + foo + "\nbar is " + JSON.stringify(bar));
             }),
-            messagerHandler = jest.fn(async ({ message }) => {
+            messageHandler = jest.fn(async ({ message }) => {
                 console.log(message);
             });
         describe("Single subscriber scenario", () => {
-            it("Should subscribe to events and process emitted data", async () => {
-                const events = new Events(redis, lightship, eventConfig);
+            itif(
+                "Should subscribe to events and process emitted data",
+                () => redis != null,
+                async (done: Function) => {
+                    const events = new Events(redis, lightship, eventConfig);
 
-                events.subscribe({
-                    "data-supplier.deliver": {
-                        group: "data-supply",
-                        handler: dataSupplierHandler
-                    },
-                    messager: {
-                        unbalanced: true,
-                        handler: messagerHandler
-                    }
-                });
+                    events.subscribe({
+                        messager: {
+                            unbalanced: true,
+                            handler: messageHandler
+                        },
+                        "data-supplier.deliver": {
+                            group: "data-supply",
+                            handler: deliverHandler
+                        }
+                    });
 
-                await events.start();
+                    await events.start();
 
-                await events.emit({
-                    type: "messager",
-                    data: { message: "This is a very cool message" }
-                });
-                await events.emit({
-                    type: "data-supplier.deliver",
-                    data: {
-                        foo: "bar",
-                        bar: { 1: "one", 2: "two", 3: "three" }
-                    }
-                });
+                    // wait for events to set all neccesary valiables e.g. last unbalanced id
+                    await sleep(200);
 
-                await sleep(1000);
+                    await events.emit({
+                        type: "messager",
+                        data: { message: "This is a very cool message" }
+                    });
+                    await events.emit({
+                        type: "data-supplier.deliver",
+                        data: {
+                            foo: "bar",
+                            bar: { 1: "one", 2: "two", 3: "three" }
+                        }
+                    });
 
-                expect(dataSupplierHandler).toHaveBeenCalledTimes(1);
-                expect(messagerHandler).toHaveBeenCalledTimes(1);
-            });
+                    await sleep(200);
+
+                    expect(messageHandler).toHaveBeenCalledTimes(1);
+                    expect(deliverHandler).toHaveBeenCalledTimes(1);
+                    done();
+                }
+            );
         });
 
         const sumHandler = jest.fn(async ({ info, numbers }) => {
-                console.log(info);
                 const sum = numbers.reduce((acc: number, n: number) => acc + n, 0);
-                console.log("Sum of numbers is " + sum);
+                console.log(info + "\nSum of numbers is " + sum);
             }),
             sumLogHandler = jest.fn(async (data) => {
                 console.log("This event will be logged\n" + "The data object is:\n" + JSON.stringify(data));
             });
         describe("Multiple subscribers scenario", () => {
-            it("Should subscribe to the same stream and process event for each subscription", async () => {
-                const events = new Events(redis, lightship, eventConfig);
+            itif(
+                "Should subscribe to the same stream and process event for each subscription",
+                () => redis != null,
+                async (done: Function) => {
+                    const events = new Events(redis, lightship, eventConfig);
 
-                events.subscribe({
-                    "calc.sum": {
-                        group: "calc-sum",
-                        handler: sumHandler,
-                        schema: calcSchema
-                    },
-                    "calc.*": {
-                        group: "calc-sum",
-                        handler: sumLogHandler
-                    }
-                });
-                await events.start();
+                    events.subscribe({
+                        "calc.sum": {
+                            group: "calc-sum",
+                            handler: sumHandler,
+                            schema: calcSchema
+                        },
+                        "calc.*": {
+                            group: "calc-sum",
+                            handler: sumLogHandler
+                        }
+                    });
+                    await events.start();
 
-                await events.emit({
-                    type: "calc.sum",
-                    data: {
-                        info: "Calculate a sum",
-                        numbers: [1, 2, 3, 4, 5]
-                    }
-                });
-                await events.emit({
-                    type: "calc.sum",
-                    data: {
-                        info: "Calculate another sum",
-                        numbers: [5, 6, 7, 8, 9]
-                    }
-                });
-                await sleep(1000);
+                    await sleep(200);
 
-                expect(sumHandler).toHaveBeenCalledTimes(2);
-                expect(sumLogHandler).toHaveBeenCalledTimes(2);
-            });
-        });
+                    await events.emit({
+                        type: "calc.sum",
+                        data: {
+                            info: "Calculate a sum",
+                            numbers: [1, 2, 3, 4, 5]
+                        }
+                    });
+                    await events.emit({
+                        type: "calc.sum",
+                        data: {
+                            info: "Calculate another sum",
+                            numbers: [5, 6, 7, 8, 9]
+                        }
+                    });
+                    await sleep(200);
 
-        const multiplyHandler = jest.fn(async ({ info, numbers }) => {
-                console.log(info);
-                const res = numbers.reduce((acc: number, n: number) => acc * n, 1);
-                console.log("Result of multiplication is " + res);
-            }),
-            multiplyLogHandler = jest.fn(async (data) => {
-                console.log("This event will be logged\n" + "The data object is:\n" + JSON.stringify(data));
-            });
-        describe("Simple handling of pending event scenario", () => {
-            it("Should create pending event and process it through subscribers", async () => {
-                const events = new Events(redis, lightship, eventConfig);
-
-                await redis.xgroup("CREATE", "cpz:events:calc.multiply", "calc-multiply", "0", "MKSTREAM");
-
-                await events.emit({
-                    type: "calc.multiply",
-                    data: {
-                        info: "This event is supposed to be pending",
-                        numbers: [1, 2, 3]
-                    }
-                });
-                await redis.xread("COUNT", 1, "STREAMS", "cpz:events:calc.multiply", "0");
-
-                await sleep(100);
-
-                events.subscribe({
-                    "calc.multiply": {
-                        group: "calc-multiply",
-                        handler: multiplyHandler
-                    },
-                    "calc.*": {
-                        group: "calc-multiply",
-                        handler: multiplyLogHandler
-                    }
-                });
-                await events.start();
-
-                await sleep(1000);
-
-                expect(multiplyHandler).toHaveBeenCalledTimes(1);
-                expect(multiplyLogHandler).toHaveBeenCalledTimes(1);
-            });
+                    expect(sumHandler).toHaveBeenCalledTimes(2);
+                    expect(sumLogHandler).toHaveBeenCalledTimes(2);
+                    done();
+                }
+            );
         });
 
         const divideHandler = jest.fn(async ({ info, numbers }) => {
@@ -182,44 +171,97 @@ describe("E2E test", () => {
                 console.log("This event will be logged\n" + "The data object is:\n" + JSON.stringify(data));
             });
         describe("Handling pending event with errors in one of the subs", () => {
-            it("Should never call the second handler", async () => {
-                const events = new Events(redis, lightship, eventConfig);
+            itif(
+                "Should never call the second handler and emit a dead-letter event",
+                () => redis != null,
+                async (done: Function) => {
+                    const events = new Events(redis, lightship, eventConfig);
 
-                events.subscribe({
-                    "calc.divide": {
-                        group: "calc-divide",
-                        handler: divideHandler,
-                        schema: calcSchema
-                    },
-                    "calc.*": {
-                        group: "calc-divide",
-                        handler: divideLogHandler
-                    }
-                });
-                await events.start();
+                    events.subscribe({
+                        "calc.divide": {
+                            group: "calc-divide",
+                            handler: divideHandler,
+                            schema: calcSchema
+                        },
+                        "calc.*": {
+                            group: "calc-divide",
+                            handler: divideLogHandler
+                        }
+                    });
+                    await events.start();
 
-                // the handler throws error if sum of numbers is < 10
-                await events.emit({
-                    type: "calc.divide",
-                    data: {
-                        info: "This is a faulty event, error is expected",
-                        numbers: [0, 0, 0, 0]
-                    }
-                });
-                // the data doesn't pass validation for the first subscriber
-                await events.emit({
-                    type: "calc.divide",
-                    data: { msg: "This message is not expected" }
-                });
-                await sleep(1000);
+                    await sleep(200);
 
-                expect(divideHandler).toHaveBeenCalledTimes(1);
-                expect(divideLogHandler).toHaveBeenCalledTimes(0);
+                    // error is thrown in the first handler since the result < 10
+                    await events.emit({
+                        type: "calc.divide",
+                        data: {
+                            info: "This is a faulty event, error is expected",
+                            numbers: [0, 0, 0, 0]
+                        }
+                    });
+                    // the data won't pass validation for the first subscriber
+                    await events.emit({
+                        type: "calc.divide",
+                        data: { msg: "This message is not expected" }
+                    });
+                    await sleep(200);
+                    const deadLetter = await redis.xread("COUNT", 1, "STREAMS", "cpz:events:dead-letter", 0);
+                    await sleep(200);
 
-                await events._receivePendingGroupMessagesTick("calc.divide", "calc-divide");
+                    expect(deadLetter == null || deadLetter == []).not.toBeTruthy();
+                    expect(divideHandler).toHaveBeenCalledTimes(1);
+                    expect(divideLogHandler).toHaveBeenCalledTimes(0);
+                    done();
+                }
+            );
+        });
 
-                await sleep(1000);
+        const logHandler = jest.fn(async ({ info }) => {
+                console.log(info);
+            }),
+            unbalancedLogHandler = jest.fn(async (data) => {
+                console.log("The data object is:\n" + JSON.stringify(data));
             });
+        describe("Subscribing after event is emitted scenario", () => {
+            itif(
+                "Should subscribe and handle the event",
+                () => redis != null,
+                async (done: Function) => {
+                    const events = new Events(redis, lightship, eventConfig);
+
+                    await events.emit({
+                        type: "grouplog",
+                        data: {
+                            info: "This event is supposed to be processed"
+                        }
+                    });
+                    await events.emit({
+                        type: "unbalancedlog",
+                        data: {
+                            info: "This event is supposed to be processed as well"
+                        }
+                    });
+
+                    events.subscribe({
+                        unbalancedlog: {
+                            unbalanced: true,
+                            handler: unbalancedLogHandler
+                        },
+                        grouplog: {
+                            group: "log",
+                            handler: logHandler
+                        }
+                    });
+                    await events.start();
+
+                    await sleep(200);
+
+                    expect(logHandler).toHaveBeenCalledTimes(1);
+                    expect(unbalancedLogHandler).toHaveBeenCalledTimes(1);
+                    done();
+                }
+            );
         });
     });
 });
