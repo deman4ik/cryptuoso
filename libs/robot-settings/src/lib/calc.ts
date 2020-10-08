1481253c13e9014c6ad96ac72068876ed3c15a3a
yourconst <58593575+yourconst@users.noreply.github.com>
2020-10-08 16:27:48 +0300

    feat: `assetDynamicDelta` linear function added (#88)
    * feat: `assetDynamicDelta` linear function added
    
    * fixed: edge values results
    
    * Update calc.ts
    
    * fix: `assetDynamicDelta`, tests
    
    * `assetDynamicDelta` function and tests refactored

--- libs/robot-settings/src/lib/calc.ts
+++ libs/robot-settings/src/lib/calc.ts
@@ -21,3 +21,32 @@ export const getUserRobotPositionVolume = (settings: UserRobotSettings, price?: 
         return calcCurrencyDynamic(settings.volumeInCurrency, price);
     } else return null;
 };
+
+export const assetDynamicDelta = (initialVolume: number, delta: number, profit: number) => {
+    if (!profit) return initialVolume;
+
+    const baseVolume = initialVolume / 2;
+
+    if (profit <= -initialVolume * delta) return roundFirstSignificant(baseVolume);
+    if (profit < initialVolume * delta) return initialVolume;
+
+    const lvl = Math.trunc((-1 + Math.sqrt(1 + 8 * (profit / (baseVolume * delta) + 1))) / 2);
+
+    return roundFirstSignificant(baseVolume * (lvl + 1));
+};
+
+/* export const assetDynamicDelta = (initialVolume: number, delta: number, profit: number) => {
+    const baseVolume = initialVolume / 2;
+    const mvd = delta * baseVolume;
+
+    if (!profit) return initialVolume;
+    if (-2 * mvd < profit && profit < 2 * mvd) return initialVolume;
+
+    const isNegative = profit < 0;
+
+    const absoluteLevel = Math.trunc((-1 + Math.sqrt(1 + 8 * (Math.abs(profit) / mvd + 1))) / 2);
+
+    let result = isNegative ? baseVolume / (absoluteLevel - 1) : baseVolume * (absoluteLevel + 1);
+
+    return roundFirstSignificant(result);
+}; */
---
+++ libs/robot-settings/src/tests/assetDynamicDelta.spec.ts
@@ -0,0 +1,84 @@
+import { assetDynamicDelta } from "../lib/calc";
+
+function makeTestFunction(initialVolume: number, delta: number) {
+    return (profit: number, expected: number) => {
+        const checked = assetDynamicDelta(initialVolume, delta, profit);
+
+        try {
+            expect(checked).toBe(expected);
+        } catch (err) {
+            throw new Error(
+                `Different results: ${checked} != ${expected}\r\n` +
+                    `On arguments: (${initialVolume}, ${delta}, ${profit})`
+            );
+        }
+    };
+}
+
+describe("assetDynamicDelta function test", () => {
+    describe("Testing with null / 0 profit provided", () => {
+        it("Should return `initialVolume` value`", () => {
+            for (let i = 0; i < 100; ++i) {
+                const initialVolume = 1 + 5 * Math.random();
+                const delta = 3 + 50 * Math.random();
+
+                const checked0 = assetDynamicDelta(initialVolume, delta, 0);
+                const checkedNull = assetDynamicDelta(initialVolume, delta, null);
+
+                let profit: number;
+
+                try {
+                    profit = 0;
+                    expect(checked0).toBe(initialVolume);
+                    profit = null;
+                    expect(checkedNull).toBe(initialVolume);
+                } catch (err) {
+                    throw new Error(
+                        `Different results: ${checked0} != ${initialVolume}\r\n` +
+                            `On arguments: (${initialVolume}, ${delta}, ${profit})`
+                    );
+                }
+            }
+        });
+    });
+
+    describe("Exact values", () => {
+        it("Should return expected results", () => {
+            const testFunction = makeTestFunction(2.6, 20);
+
+            testFunction(-2705, 1.3);
+            testFunction(-2704, 1.3);
+            testFunction(-2703, 1.3);
+            testFunction(-2401, 1.3);
+            testFunction(-2400, 1.3);
+            testFunction(-2339, 1.3);
+            testFunction(-131, 1.3);
+            testFunction(-130, 1.3);
+            testFunction(-129, 1.3);
+            testFunction(-53, 1.3);
+            testFunction(-52, 1.3);
+            testFunction(-51, 2.6);
+            testFunction(-27, 2.6);
+            testFunction(-26, 2.6);
+            testFunction(-25, 2.6);
+            testFunction(-1, 2.6);
+            testFunction(0, 2.6);
+            testFunction(1, 2.6);
+            testFunction(25, 2.6);
+            testFunction(26, 2.6);
+            testFunction(27, 2.6);
+            testFunction(51, 2.6);
+            testFunction(52, 3.9);
+            testFunction(53, 3.9);
+            testFunction(129, 3.9);
+            testFunction(130, 5.2);
+            testFunction(131, 5.2);
+            testFunction(2339, 16.9);
+            testFunction(2400, 18.2);
+            testFunction(2401, 18.2);
+            testFunction(2703, 18.2);
+            testFunction(2704, 19.5);
+            testFunction(2705, 19.5);
+        });
+    });
+});
