c96ce164be80341f21039bbf13c4d57b1bc5bbe8
Dmitriy Shatalov <deman4ik@gmail.com>
2020-11-22 16:11:36 +0300

    WIP: telegram

--- @types/telegraf.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "telegraf";
-declare module "telegraf/stage";
-declare module "telegraf/scenes/base";
-declare module "telegraf/session";
--- apps/telegram-bot/src/app/service.ts
+++ apps/telegram-bot/src/app/service.ts
@@ -1,16 +1,18 @@
 import { BaseService, BaseServiceConfig } from "@cryptuoso/service";
 import dayjs from "@cryptuoso/dayjs";
-import { Telegraf, Extra } from "telegraf";
-import Stage from "telegraf/stage";
+import { Telegraf, Extra, Stage, BaseScene } from "telegraf";
+
+import { I18n, match, reply } from "@edjopato/telegraf-i18n";
+import { TelegrafSessionRedis } from "@ivaniuk/telegraf-session-redis";
+
 const { enter, leave } = Stage;
-import Scene from "telegraf/scenes/base";
-import TelegrafI18n, { match, reply } from "telegraf-i18n";
-import Session from "telegraf-session-redis";
 
 export type TelegramBotServiceConfig = BaseServiceConfig;
 
 export default class TelegramBotService extends BaseService {
-    bot: any;
+    bot: Telegraf<any>;
+    i18n: I18n;
+    session: TelegrafSessionRedis;
     constructor(config?: TelegramBotServiceConfig) {
         super(config);
         try {
@@ -18,7 +20,33 @@ export default class TelegramBotService extends BaseService {
             this.bot.catch((err: any) => {
                 this.log.error(err);
             });
+            this.session = new TelegrafSessionRedis({
+                client: this.redis
+            });
+            this.bot.use(this.session.middleware());
+
+            this.i18n = new I18n({
+                defaultLanguage: "en",
+                useSession: true,
+                defaultLanguageOnMissing: true
+                //directory: path.resolve(process.cwd(), "state/telegram/locales")
+            });
+            this.bot.use(this.i18n.middleware());
+            // Create scene manager
+            const stage = new Stage([]);
+            stage.command("cancel", leave());
+
+            this.bot.use(async (ctx, next) => {
+                const start = dayjs.utc();
+                await next();
+                const ms = dayjs.utc().diff(start, "millisecond");
+                this.log.debug("Response time %sms", ms);
+            });
+            this.bot.use(stage.middleware());
+
             this.bot.hears(/(.*?)/, this.defaultHandler.bind(this));
+
+            this.addOnStartHandler(this.onServiceStart);
         } catch (err) {
             this.log.error("Error in TelegramBotService constructor", err);
         }
@@ -27,7 +55,7 @@ export default class TelegramBotService extends BaseService {
     async onServiceStart() {
         if (process.env.NODE_ENV === "production") {
             await this.bot.telegram.setWebhook(process.env.BOT_HOST);
-            await this.bot.startWebhook("/", null, process.env.PORT);
+            await this.bot.startWebhook("/", null, +process.env.PORT);
             this.log.warn("Bot in production mode!");
         } else if (process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "development") {
             await this.bot.telegram.deleteWebhook();
