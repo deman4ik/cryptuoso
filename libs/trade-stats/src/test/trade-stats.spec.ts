8b579c5f841a635e17a1f806bb8a2c64fb3766b7
Dmitriy Shatalov <deman4ik@gmail.com>
2021-03-29 21:54:56 +0300

    WIP: trade-stats

--- libs/trade-stats/src/test/trade-stats.spec.ts
+++ libs/trade-stats/src/test/trade-stats.spec.ts
@@ -1,16 +1,21 @@
 import { TradeStatsCalc } from "../lib/trade-stats";
 import { positions } from "./data/positions";
+import { result } from "./data/results";
+import util from "util";
+import fs from "fs";
 
 describe("Test 'trade-stats'", () => {
-    // Refactored to automatically round every value
     describe("Test calc with no previous statistics", () => {
         it("Should  calculate statistics", async () => {
             const tradeStatsCalculator = new TradeStatsCalc(positions, {
-                type: "robotsAggr"
+                type: "robot"
             });
             const stats = tradeStatsCalculator.calculate();
-            //console.log(stats.fullStats);
-            console.log(stats.periodStats.year["2021"]);
+
+            expect(stats).toEqual(result);
+            // console.log(util.inspect(stats.periodStats.month, false, null, true));
+            // let data = JSON.stringify(stats);
+            //fs.writeFileSync("stats.json", data);
         });
     });
 });
