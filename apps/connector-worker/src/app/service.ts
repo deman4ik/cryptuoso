5653dca4b8b9982dbe4003caf83a58e292b3c4db
Dmitriy Shatalov <deman4ik@gmail.com>
2022-02-04 15:45:54 +0300

    fix: connector-worker order meta

--- apps/connector-worker/src/app/service.ts
+++ apps/connector-worker/src/app/service.ts
@@ -159,7 +159,7 @@ export default class ConnectorRunnerService extends BaseService {
                 ${order.remaining || null}, ${order.executed || null}, ${order.fee || null},
                 ${order.lastCheckedAt || null}, ${JSON.stringify(order.params) || null},
                 ${order.error || null}, ${JSON.stringify(order.nextJob) || null},
-                ${JSON.stringify(order.meta) || null}
+                ${JSON.stringify(order.meta) || JSON.stringify({})}
             );
             `);
     };
@@ -181,7 +181,7 @@ export default class ConnectorRunnerService extends BaseService {
          error = ${JSON.stringify(order.error) || null},
          next_job = ${JSON.stringify(order.nextJob) || null},
          info = ${JSON.stringify(order.info) || null},
-         meta = ${JSON.stringify(order.meta) || null}
+         meta = ${JSON.stringify(order.meta) || JSON.stringify({})}
          WHERE id = ${order.id}
         `);
         } catch (error) {
