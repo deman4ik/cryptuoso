7648c6b670f97b045a26f0b0f34077f154a9ce01
Dmitriy Shatalov <deman4ik@gmail.com>
2022-02-24 16:17:46 +0300

    fix: user robot dedicated | DEPLOY | ALL |

--- apps/user-robot-dedicated/src/main.ts
+++ apps/user-robot-dedicated/src/main.ts
@@ -1,18 +1,14 @@
 import { UserRobotBaseService as Service } from "@cryptuoso/user-robot";
 import log from "@cryptuoso/logger";
 
-if (!process.env.EXCHANGE) {
-    log.error("No env vars");
-} else {
-    const service = new Service();
+const service = new Service();
 
-    async function start() {
-        try {
-            await service.startService();
-        } catch (error) {
-            log.error(`Failed to start service ${process.env.SERVICE}`, error);
-            process.exit(1);
-        }
+async function start() {
+    try {
+        await service.startService();
+    } catch (error) {
+        log.error(`Failed to start service ${process.env.SERVICE}`, error);
+        process.exit(1);
     }
-    start();
 }
+start();
