f3bf98c046b53143502d164d29498d496c3054ca
Dmitriy Shatalov <deman4ik@gmail.com>
2022-04-07 22:35:09 +0300

    fix: trade-stats | DEPLOY | trade-stats-worker portfolio-manager backtester-worker |

--- libs/trade-stats/src/lib/trade-stats.ts
+++ libs/trade-stats/src/lib/trade-stats.ts
@@ -453,9 +453,9 @@ export class TradeStatsCalc implements TradeStats {
         stats.payoffRatio = Math.abs(divide(stats.avgGrossProfit, stats.avgGrossLoss));
 
         if (this.hasBalance) {
-            stats.percentNetProfit = (stats.netProfit / stats.currentBalance) * 100;
-            stats.percentGrossProfit = (stats.grossProfit / stats.currentBalance) * 100;
-            stats.percentGrossLoss = (stats.grossLoss / stats.currentBalance) * 100;
+            stats.percentNetProfit = (stats.netProfit / stats.initialBalance) * 100;
+            stats.percentGrossProfit = (stats.grossProfit / stats.initialBalance) * 100;
+            stats.percentGrossLoss = (stats.grossLoss / stats.initialBalance) * 100;
             stats.amountProportion = 100 / stats.percentMaxDrawdown;
 
             stats.recoveryFactor = divide(stats.percentNetProfit, stats.percentMaxDrawdown);
@@ -540,9 +540,9 @@ export class TradeStatsCalc implements TradeStats {
         stats.payoffRatio = Math.abs(divide(stats.avgGrossProfit, stats.avgGrossLoss));
 
         if (this.hasBalance) {
-            stats.percentNetProfit = (stats.netProfit / stats.currentBalance) * 100;
-            stats.percentGrossProfit = (stats.grossProfit / stats.currentBalance) * 100;
-            stats.percentGrossLoss = (stats.grossLoss / stats.currentBalance) * 100;
+            stats.percentNetProfit = (stats.netProfit / stats.initialBalance) * 100;
+            stats.percentGrossProfit = (stats.grossProfit / stats.initialBalance) * 100;
+            stats.percentGrossLoss = (stats.grossLoss / stats.initialBalance) * 100;
             stats.recoveryFactor = divide(stats.percentNetProfit, stats.percentMaxDrawdown);
         } else {
             stats.recoveryFactor = divide(stats.netProfit, stats.maxDrawdown) * -1;
