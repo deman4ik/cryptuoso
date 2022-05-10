df9af3e4835606ae105bfb08edf2a8e0909fd5c3
Dmitriy Shatalov <deman4ik@gmail.com>
2022-05-10 15:46:46 +0300

    fix: redis conn str | DEPLOY | ALL |

--- libs/service/src/lib/utils.ts
+++ libs/service/src/lib/utils.ts
@@ -8,18 +8,31 @@ export function parseURL(url: string) {
         parsed = urllibParse(url, true, true);
     }
 
-    const result: any = { tls: true };
+    const result: any = {};
+    if (parsed.protocol === "rediss:") {
+        result.tls = true;
+    }
     if (parsed.auth) {
         const index = parsed.auth.indexOf(":");
         result.username = index === -1 ? parsed.auth : parsed.auth.slice(0, index);
         result.password = index === -1 ? "" : parsed.auth.slice(index + 1);
     }
-
-    if (parsed.host) {
-        result.host = parsed.hostname;
-    }
-    if (parsed.port) {
-        result.port = parsed.port;
+    if (parsed.protocol === "redis-sentinel:") {
+        result.sentinels = [
+            {
+                host: parsed.hostname,
+                port: parsed.port
+            }
+        ];
+        result.name = parsed.query.sentinelMasterId;
+        result.role = "master";
+    } else {
+        if (parsed.host) {
+            result.host = parsed.hostname;
+        }
+        if (parsed.port) {
+            result.port = parsed.port;
+        }
     }
 
     return result;
